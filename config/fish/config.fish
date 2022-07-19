99cfe90279b6e28a55ef2c34f92fda07c2dd6a88
Tyler <t.angley@protonmail.com>
2022-07-19 11:14:06 -0400

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,16 @@
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Add bin to home dir
 fish_add_path $HOME_DIR/.local/bin
+
+# Repeat last command as sudo
+function pls
+	eval command sudo $history[1]
+end
+
+# Open fishrc
+alias fishrc="nvim ~/.config/fish/config.fish"
+
