fb3a8ecaeb582b004c142b268a72f1fcbede0ab4
tyler <tyler@rose>
2023-11-23 06:28:56 -0500

    remove move shortcut

--- config/nvim/lua/config/remap.lua
+++ config/nvim/lua/config/remap.lua
@@ -4,10 +4,6 @@ vim.keymap.set("n", "<leader>ee", "<cmd>Ex<CR>")
 -- Bring next line to end of line without moving cursor
 vim.keymap.set("n", "J", "mzJ`z")
 
--- Move highlighted things around in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 -- Paste/delete and put underlying text in void register
 vim.keymap.set("x", "<leader>p", '"_dP')
 vim.keymap.set("n", "<leader>d", '"_d')
