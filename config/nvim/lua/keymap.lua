7f9c6ab10c45a6d09953537767f724fe08f9c3fe
constellationz <bluemoon40@pm.me>
2024-03-30 05:46:39 -0400

    update neovim config

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -23,28 +23,28 @@ do
 	-- Save current buffer
 	vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
 
-	-- Close/quit view
-	vim.keymap.set("n", "<C-q>", "<cmd>close<CR>")
-
-	-- Next buffer, previous buffer
+	-- Switch buffers 
 	vim.keymap.set("n", "<C-n>", "<cmd>bnext<CR>")
 	vim.keymap.set("n", "<C-p>", "<cmd>bprev<CR>")
-
-	-- New tab with current buffer
-	vim.keymap.set("n", "<C-t>", "<cmd>tabedit %<CR>")
-
-	-- Next tab, previous tab
-	vim.keymap.set("n", "<C-Tab>", "<cmd>tabnext<CR>")
-	vim.keymap.set("n", "<C-S-Tab>", "<cmd>tabprev<CR>")
-
-	-- Show git status (plugin)
-	vim.keymap.set("n", "<C-g>", "<cmd>Git<CR>")
 
 	-- Format with Neoformat (plugin)
 	vim.keymap.set("n", "<C-f>", "<cmd>Neoformat<CR>")
 
+	-- Switch tabs
+	vim.keymap.set("n", "<C-Tab>", "<cmd>tabnext<CR>")
+	vim.keymap.set("n", "<C-S-Tab>", "<cmd>tabprev<CR>")
+
+	-- Open new tab
+	vim.keymap.set("n", "<leader>t", "<cmd>tabedit<CR>")
+
+	-- Close/quit view
+	vim.keymap.set("n", "<leader>q", "<cmd>close<CR>")
+
 	-- Explorer
 	vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
+
+	-- Show version control (plugin)
+	vim.keymap.set("n", "<leader>v", "<cmd>Git<CR>")
 
 	-- Live grep (plugin)
 	vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<CR>")
