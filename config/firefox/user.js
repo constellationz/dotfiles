9d0900981a4bb7912ee15fd2c539fc1f55e43e2c
constellationz <bluemoon40@pm.me>
2024-05-19 05:35:47 -0400

    add firefox configs

---
+++ config/firefox/user.js
@@ -0,0 +1,7 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("userChrome.Tabs.Option7.Enabled", true);
+user_pref("userChrome.DarkTheme.Tabs.Borders.Saturation.Medium.Enabled", true);
+user_pref("userChrome.TabSeparators.Saturation.Low.Enabled", true);
+user_pref("userChrome.Menu.Size.Large.Enabled", true);
+user_pref("userChrome.Menu.Icons.Regular.Enabled", true);
+user_pref("userChrome.Tabs.SelectedTabIndicator.Enabled", true);
