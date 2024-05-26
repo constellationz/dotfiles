d3b3938c56089b80a622d04d3ea338573af3a99c
constellationz <bluemoon40@pm.me>
2024-05-26 10:51:02 -0400

    update firefox style

--- config/firefox/user.js
+++ config/firefox/user.js
@@ -4,4 +4,4 @@ user_pref("userChrome.DarkTheme.Tabs.Borders.Saturation.Medium.Enabled", true);
 user_pref("userChrome.TabSeparators.Saturation.Low.Enabled", true);
 user_pref("userChrome.Menu.Size.Large.Enabled", true);
 user_pref("userChrome.Menu.Icons.Regular.Enabled", true);
-user_pref("userChrome.Tabs.SelectedTabIndicator.Enabled", true);
+user_pref("userChrome.Tabs.SelectedTabIndicator.Enabled", false);
