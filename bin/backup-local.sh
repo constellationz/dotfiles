c2d301fb1c649654cd21bdde9bf48288ceb6c0d2
Tyler <tyler@rosemary.local>
2025-08-20 21:42:10 -0400

    fix sync error

--- bin/backup-local.sh
+++ bin/backup-local.sh
@@ -13,10 +13,10 @@
 mkdir -p ~/Desktop/rsynclog
 
 # One-directional lfs backup
-rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --archive --verbose --compress --progress --partial ~/Desktop/lfs tyler@hopper.local:/media/hdd/tyler/lfs | tee -a ~/Desktop/rsynclog/lfs-hopper-log.txt
-echo Desktop/lfs forward backup complete
+rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --archive --verbose --compress --progress --partial ~/Desktop/lfs/ tyler@hopper.local:/media/hdd/tyler/lfs/ | tee -a ~/Desktop/rsynclog/lfs-hopper-log.txt
+echo Desktop/lfs/ forward backup complete
 
 # One-directional sync (using forward deletion)
 # Make sure to do dry-run before initiating actual transfer
-rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --dry-run --archive --delete-before --verbose --compress --progress --partial ~/Desktop/sync tyler@hopper.local:/media/hdd/tyler/sync | tee -a ~/Desktop/rsynclog/sync-hopper-log.txt
-echo Desktop/sync forward sync complete
+rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --archive --delete-before --verbose --compress --progress --partial ~/Desktop/sync/ tyler@hopper.local:/media/hdd/tyler/sync/ | tee -a ~/Desktop/rsynclog/sync-hopper-log.txt
+echo Desktop/sync/ forward sync complete
--- bin/backup-wg.sh
+++ bin/backup-wg.sh
@@ -17,10 +17,10 @@
 mkdir -p ~/Desktop/rsynclog
 
 # One-directional lfs backup
-rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --archive --verbose --compress --progress --partial --bwlimit=500 ~/Desktop/lfs tyler@hopper.wg:/media/hdd/tyler/lfs | tee -a ~/Desktop/rsynclog/lfs-hopper-log.txt
-echo Desktop/lfs forward backup complete
+rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --archive --verbose --compress --progress --partial --bwlimit=500 ~/Desktop/lfs/ tyler@hopper.wg:/media/hdd/tyler/lfs/ | tee -a ~/Desktop/rsynclog/lfs-hopper-log.txt
+echo Desktop/lfs/ forward backup complete
 
 # One-directional sync (using forward deletion)
 # Make sure to do dry-run before initiating actual transfer
-rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --dry-run --archive --delete-before --verbose --compress --progress --partial --bwlimit=500 ~/Desktop/sync tyler@hopper.wg:/media/hdd/tyler/sync | tee -a ~/Desktop/rsynclog/sync-hopper-log.txt
-echo Desktop/sync forward sync complete
+rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --archive --delete-before --verbose --compress --progress --partial --bwlimit=500 ~/Desktop/sync/ tyler@hopper.wg:/media/hdd/tyler/sync/ | tee -a ~/Desktop/rsynclog/sync-hopper-log.txt
+echo Desktop/sync/ forward sync complete
