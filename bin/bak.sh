778284c9fb61d3664c0cc9408b229ef9d73e12a0
Tyler <tyler@rosemary.local>
2026-01-31 16:19:31 -0500

    change bak.sh dest

--- bin/bak.sh
+++ bin/bak.sh
@@ -18,7 +18,7 @@ if [ -z "$dest" ]; then
   echo "usage: $(basename $0) hopper.wg0"
   exit 1
 fi
-destdir="$(hostname -s)-bak"
+destdir="$(hostname -s)"
 
 # log
 echo "syncing to device $dest"
@@ -29,5 +29,5 @@ mkdir -p ~/rsynclog
 
 # One-directional sync (using forward deletion)
 # Make sure to do dry-run before initiating actual transfer
-rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --archive --delete-before --verbose --compress --progress --partial ~/sync/ tyler@$dest:/media/hdd/tyler/$destdir/ | tee -a ~/rsynclog/sync-hopper-log.txt
+rsync --rsh="ssh -p 1183 -i ~/.ssh/hopper" --archive --delete-after --exclude=".hist" --verbose --compress --progress --partial ~/sync/ tyler@$dest:/home/$destdir/ | tee -a ~/rsynclog/sync-hopper-log.txt
 echo "~/sync/ forward sync complete"
