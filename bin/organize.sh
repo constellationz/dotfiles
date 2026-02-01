1b70f1036331614d30c461f696869ff3175fcd2e
Tyler <tyler@blippy.local>
2026-02-01 04:10:51 -0500

    add organize.sh

---
+++ bin/organize.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DESKTOP="$HOME/Desktop"
+SYNC="$DESKTOP/sync"
+
+mv $DESKTOP/*.mp4 $SYNC/pictures 
+mv $DESKTOP/*.png $SYNC/pictures 
+mv $DESKTOP/*.mov $SYNC/pictures 
+mv $DESKTOP/*.pdf $SYNC/pdf 
+mv $DESKTOP/*.dmg $SYNC/dmg
+mv $DESKTOP/*.jpeg $SYNC/pictures 
