1f6e2aa61c5494c3b95fd42e094689d3e003fab9
Tyler <tyler@blippy.local>
2026-02-02 11:04:36 -0500

    fix literals

--- bin/organize.sh
+++ bin/organize.sh
@@ -3,9 +3,9 @@
 DESKTOP="$HOME/Desktop"
 SYNC="$DESKTOP/sync"
 
-mv $DESKTOP/*.mp4 $SYNC/pictures 
-mv $DESKTOP/*.png $SYNC/pictures 
-mv $DESKTOP/*.mov $SYNC/pictures 
-mv $DESKTOP/*.pdf $SYNC/pdf 
-mv $DESKTOP/*.dmg $SYNC/dmg
-mv $DESKTOP/*.jpeg $SYNC/pictures 
+mv "$DESKTOP"/*.dmg "$SYNC"/dmg
+mv "$DESKTOP"/*.jpeg "$SYNC"/pictures
+mv "$DESKTOP"/*.mov "$SYNC"/pictures
+mv "$DESKTOP"/*.mp4 "$SYNC"/pictures
+mv "$DESKTOP"/*.png "$SYNC"/pictures
+mv "$DESKTOP"/*.pdf "$SYNC"/pdf
